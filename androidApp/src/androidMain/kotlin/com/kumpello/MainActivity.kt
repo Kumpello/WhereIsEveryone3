d06052b3e8c17dc15eb359db05e2acf49f875cc2
kumpello <bleidd04@gmail.com>
2023-09-17 18:56:53 +0200

    *changed naming in project

--- androidApp/src/androidMain/kotlin/com/kumpello/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/kumpello/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myapplication
+package com.kumpello
 
 import MainView
 import android.os.Bundle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "MyApplication"
+rootProject.name = "WhereIsEveryone"
 
 include(":androidApp")
 include(":shared")
